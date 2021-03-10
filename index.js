0d09332ec41a9f4e1a9cfa3f74177d42f6bd2968
Akarsh Shukla <akarshshukla19@yahoo.com>
2021-03-10 04:05:15 +0000

    Done with assignment

---
+++ index.js
@@ -0,0 +1,74 @@
+function add(x, y) {
+    x + y;
+}
+console.log(add(1,2));
+
+function subtract(x, y) {
+    x - y;
+}
+console.log(subtract(2,1));
+
+function multiply(x, y) {
+    x * y;
+}
+console.log(multiply(1,2));
+
+function divide(x, y) {
+    x / y;
+}
+console.log(divide(1,2));
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1,2));
+
+function subtract(a, b) {
+    return a - b;
+}
+console.log(subtract(1,2));
+
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(1,2));
+
+function divide(a, b) {
+    return a / b;
+}
+console.log(divide(1,2));
+
+function increment(n) {
+    return n += 1;
+}
+console.log(increment(5));
+
+function decrement(n) {
+    return n -= 1;
+}
+console.log(decrement(5));
+
+function makeInt(n) {
+   return parseInt(n);
+}
+console.log(makeInt('2'));
+
+function makeInt(n) {
+   return parseInt(n, 10);
+}
+console.log(makeInt('0 * 2328', 10));
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+console.log(preserveDecimal('80.12399'));
+
+
+
+
+
+
+
+
+
+
